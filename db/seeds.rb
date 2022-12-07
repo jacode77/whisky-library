9e73cdcbd671b86cf981fed97d5b05fa2f553914
Jacqui <jt.cope1@gmail.com>
2022-12-07 22:57:57 +0800

    Adds brand, category, country models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+category = ["Single Malt Whisky", "Grain Whisky", "Blended Whisky" ]
+
+if Category.count == 0 
+    category.each do |c|
+        Category.create(name: c)
+        puts "Created #{c} type"
+    end
+end
+
+countries = ["Japan", "America", "Ireland", "Scotland", "England", "Taiwan", "Canada", "India", "Australia"]
+
+if Country.count == 0
+    countries.each do |c|
+        Country.create(name: c)
+        puts "Created #{c} country listing"
+    end
+end
+
+brands = ["Glenfiddich", "Glenmorangie", "Highland Park",  "Macallan", "Hakushu", "Hibiki", "Yamazaki", "William Larue Weller", "Johnnie Walker"]
+
+if Brand.count == 0
+    brands.each do |b|
+        Brand.create(name: b)
+        puts "Created #{b}"
+    end
+end
