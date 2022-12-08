2c8d284629cd9bfabe3fdf4747b41f20230aa77c
Jacqui <jt.cope1@gmail.com>
2022-12-08 11:06:00 +0800

    Generates listings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'listings/index'
+  get 'listings/show'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "pages#home"
   get "/restricted", to: "pages#restricted"
+  get "/listings", to: "listings#index"
 end
