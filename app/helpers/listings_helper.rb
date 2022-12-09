e2d421ad5681b2b05c2fcae74975af6bdf20496a
Jacqui <jt.cope1@gmail.com>
2022-12-09 11:47:24 +0800

    Sets up index & show page with routes for listings

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,10 @@
 module ListingsHelper
+
+    def format_brand(brand)
+
+    end
+
+    def format_price(price)
+        "$" + (price / 100).to_s
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  get 'listings/index'
-  get 'listings/show'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "pages#home"
   get "/restricted", to: "pages#restricted"
-  get "/listings", to: "listings#index"
+  get "listings", to: "listings#index", as: "listings"
+  post "listings", to: "listings#create"
+  get "listings/new", to: "listings#new", as: "new_listing"
+  get "listings/:id", to: "listings#show", as: "listing"
+  put "listings/:id", to: "listings#update"
+  patch "listings/:id", to: "listings#update"
+  delete "listing/:id", to: "listing#destroy"
+  get "listings/:id/edit", to: "listings#edit", as: "edit_listing"
 end
